6880a7f4b45ae016d27ee94aaa129b651074e0a4
Kevin Wang <kevwang@f6r10s14.clusters.42paris.fr>
2025-08-11 20:14:03 +0200

    modif changement echo strcmp par strncmp pour verif uniquemnt 4char, car 'echo   ' doit marcher

--- minishellv02/cd_test_4.c
+++ minishellv02/cd_test_4.c
@@ -479,7 +479,9 @@ int    homemade_cd(char **tab, t_all **all)
 
 int	is_echo(char *str)
 {
-	if (ft_strcmp(str, "echo") == 0)
+	// if (ft_strcmp(str, "echo") == 0)
+	// 	return (1);
+	if (ft_strncmp(str, "echo", 4) == 0)
 		return (1);
 	return (0);
 }
